86aaea275675befc0543a5be87fde073a553138d
ahm-fahim <contactahmfahim@gmail.com>
2025-01-30 12:54:33 +0600

    users and profile info table created

--- src/drizzle/drizzle.module.ts
+++ src/drizzle/drizzle.module.ts
@@ -1,4 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
+import * as schema from './schema';
+import { Pool } from 'pg';
 
-@Module({})
+export const DRIZZLE = Symbol('drizzle-connection');
+
+@Module({
+  providers: [
+    {
+      provide: DRIZZLE,
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const databaseURL = configService.get<string>('DATABASE_URL');
+
+        if (!databaseURL) {
+          throw new Error(
+            'DATABASE_URL is not defined in environment variables',
+          );
+        }
+
+        const pool = new Pool({
+          connectionString: databaseURL,
+          ssl: { rejectUnauthorized: false }, // Ensuring SSL works properly
+        });
+
+        return drizzle(pool, { schema }) as NodePgDatabase<typeof schema>;
+      },
+    },
+  ],
+  exports: [DRIZZLE],
+})
 export class DrizzleModule {}
